8c1cbcca59dcb4cffbcbca3607de50cf27f3f690
Tawhid Islam <xcode.dev@yahoo.com>
2024-12-29 11:43:41 +0600

    Menu Done (Not Responsive)

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 // Navbar Code Imported
 import navInit from "./navbar.js";
 import lenisInit from "./lenis.js";
+import menuBtnInit from "./menu-btn.js";
 navInit(); // Initialize navigation
 lenisInit(); // Initialize lenis
+menuBtnInit(); // Initialize Menu Button
